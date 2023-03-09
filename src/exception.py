cdc0d1e7aa0dd5e1f2e2cc143616a88d55823a80
Rohit Bharti <rohit.bharti8211@gmail.com>
2023-03-09 17:40:59 +0530

    adding notebook folder and data

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,4 @@
 import sys
-import logging
 
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -17,12 +16,4 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
-    
-    
-if __name__ == '__main__':
-    try:
-            a = 1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e, sys)
             
